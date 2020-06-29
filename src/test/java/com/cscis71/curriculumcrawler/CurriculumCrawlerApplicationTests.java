8ad4339658a9ec8600695c6a4815b8232ef8d746
hemalsid@gmail.com <shemal@cloudhealthtech.com>
2020-06-29 16:57:27 -0400

    Add boiler plate ES code

---
+++ src/test/java/com/cscis71/curriculumcrawler/CurriculumCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscis71.curriculumcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
