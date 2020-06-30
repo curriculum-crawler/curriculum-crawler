583911d6a13c7082396c5d0989d338608e438450
hemalsid@gmail.com <shemal@cloudhealthtech.com>
2020-06-29 20:50:06 -0400

    Move unit test

--- src/test/java/com/cscis71/curriculumcrawler/CurriculumCrawlerApplicationTests.java
+++ src/test/java/com/cscis71/curriculumcrawler/CurriculumCrawlerApplicationTests.java
@@ -1,7 +1,12 @@
 package com.cscis71.curriculumcrawler;
 
+import com.cscis71.curriculumcrawler.model.Curriculum;
+import com.cscis71.curriculumcrawler.repository.CurriculumRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class CurriculumCrawlerApplicationTests {
@@ -10,4 +15,19 @@ class CurriculumCrawlerApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private CurriculumRepository curriculumRepository;
+
+	@Test
+	public void addNewCurriculum() {
+		Curriculum curriculum = new Curriculum();
+		curriculum.setId("test_id");
+		curriculum.setTitle("Test Curriculum");
+		curriculumRepository.save(curriculum);
+
+		Optional<Curriculum> retrieved = curriculumRepository.findById("test_id");
+		if(retrieved.isPresent()) {
+			assert curriculum.getTitle().equals(retrieved.get().getTitle());
+		}
+	}
 }
